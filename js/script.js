7f3253a16952e4ffacf6825d602a79996b1524b2
Vitaliy Emelianov <vitaliy030589@gmail.com>
2017-01-21 17:41:53 +0300

    menu tesch

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
 
 	$('.mainSlider').slick({
@@ -24,8 +25,81 @@ $(document).ready(function(){
 
 	});
 
-	$(window).scroll(function(){
-		console.log(window.location.hash);
+//MODAL BEGIN
+	$('.modalFormButton').click(function(){
+		$('.modalWindowWrapper').fadeIn('fast');
+		$('.modalWindow').fadeIn('fast');
 	});
+
+	$('.modalWindowClose').click(function(){
+		$('.modalWindowWrapper').fadeOut('fast');
+		$('.modalWindow').fadeOut('fast');
+	})
+//MODAL END
+
+	
+
+	$('.secondTabs').each(function(){
+			$(this).addClass('displayTabsNone');
+			$(this).removeClass('tabs_active');
+		});
+	
+	$('.teachContent').each(function(){
+			$(this).addClass('displayTabsNone');
+			$(this).removeClass('tabs_active');
+		});	
+	
+	$('#parikmaherskoe').addClass('displayTabs');
+	$('#ParikPerv').removeClass('displayTabsNone');
+	$('#parikmaherskoe').removeClass('displayTabsNone');
+
+	$('.teachContentZaglushka').css('display', 'none');
+
+	$('.firstTabs_tab').click(function(){
+		$('.firstTabs_tab').removeClass('tabs_active');
+		$(this).addClass('tabs_active');
+
+		$('.teachContentZaglushka').css('display', 'block');
+
+		console.log($(this).attr('idOpen'));
+		var attrOpenLink = $(this).attr('idOpen');
+
+		$('.secondTabs').each(function(){
+			$(this).addClass('displayTabsNone');
+		});
+
+		$('.teachContent').each(function(){
+			$(this).addClass('displayTabsNone');
+		});
+		
+		$('.secondTabs').each(function(){
+			console.log($(this).attr('id'));
+			if($(this).attr('id') == attrOpenLink) {
+				$(this).removeClass('displayTabsNone');
+				$(this).addClass('displayTabs');
+			}
+		});
+	});
+
+	$('.secondTabs_tab').click(function(){
+		$('.secondTabs_tab').removeClass('tabs_active');
+		$(this).addClass('tabs_active');
+
+		var attrOpenLink = $(this).attr('idOpen');
+
+		$('.teachContentZaglushka').css('display', 'none');
+
+		$('.teachContent').each(function(){
+			$(this).addClass('displayTabsNone');
+		});
+
+		$('.teachContent').each(function(){
+			if($(this).attr('id') == attrOpenLink) {
+				$(this).removeClass('displayTabsNone');
+			}
+		});
+
+	});
+
 
 });
